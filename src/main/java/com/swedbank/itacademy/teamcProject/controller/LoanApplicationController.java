9d77b76e9fbf94c1bb5c3180b1b70e87d07997ee
Vilius Zukauskas <Vilius Zukauskas>
2017-02-28 14:30:07 +0200

    Cors filter fix

--- src/main/java/com/swedbank/itacademy/teamcProject/controller/LoanApplicationController.java
+++ src/main/java/com/swedbank/itacademy/teamcProject/controller/LoanApplicationController.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 /**
  * Created by benas on 17.2.24.
  */
-@CrossOrigin(origins = "https://frozen-hamlet-97469.herokuapp.com", maxAge = 3600) 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class LoanApplicationController {
 
@@ -18,12 +18,10 @@ public class LoanApplicationController {
     @Autowired
     private LoanService loansService;
 
-
     @RequestMapping(value = "/loans", method = RequestMethod.GET)
     public Iterable<Loans> test() {
         return loansService.getLoans();
     }
-
 
     @RequestMapping(value = "/loans/new", method = RequestMethod.POST)
 
